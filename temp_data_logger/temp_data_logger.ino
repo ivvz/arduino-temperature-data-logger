d63327ae26d328e82dca3f128c28e67190197ff8
ivvz <ivanwzh.iqm@gmail.com>
2021-11-17 22:55:34 -0600

    funciona un boton y se crearon funciones para el termopar

--- temp_data_logger/temp_data_logger.ino
+++ temp_data_logger/temp_data_logger.ino
@@ -7,22 +7,72 @@ int thermoDO = 4;
 int thermoCS = 5;
 int thermoCLK = 6;
 
+int buttonCreate = 2;
+int buttonClose = 3;
+int led =7;
+
+unsigned long termocoupleTime;
+unsigned long readTime = 500;
+
+float temperature;
+
+int lastStateCreateButton;
+int lastStateCloseButton;
+
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 void setup() {
   Serial.begin(9600);
 
-  Serial.println("MAX6675 test");
+  pinMode(buttonCreate, INPUT_PULLUP); 
+  pinMode(buttonClose, INPUT_PULLUP);
+  digitalWrite(buttonCreate, HIGH);
+  digitalWrite(buttonClose, HIGH);
+
+  pinMode(led, OUTPUT);
+  
+
+
+  lastStateCreateButton = digitalRead(buttonCreate);
+  lastStateCloseButton = digitalRead(buttonClose);
+  
+  Serial.println("MAX6675 correct");
   // wait for MAX chip to stabilize
   delay(500);
+  
 }
 
 void loop() {
-  // basic readout test, just print the current temp
+
+  if(millis() - termocoupleTime >= readTime){
+    continuousTermocoupleRead();  
+    }
+
+  int buttonState = digitalRead(buttonCreate);
   
+  if(digitalRead(buttonState == LOW)){
+   //Serial.println("no apretaste el boton");
+   digitalWrite(led, LOW);
+  } else{
+    digitalWrite(7,HIGH);
+    Serial.println("creatting file");
+    delay(1000);
+  }
+
+
+
+}
+
+void continuousTermocoupleRead(){
    Serial.print("C = "); 
-   Serial.println(thermocouple.readCelsius());
-    
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-   delay(1000);
+   readTemperature();
+   //delay(250);
+   termocoupleTime = millis();
+   
+}
+
+void readTemperature(){
+  temperature = thermocouple.readCelsius();
+  Serial.println(temperature);
+  
 }
