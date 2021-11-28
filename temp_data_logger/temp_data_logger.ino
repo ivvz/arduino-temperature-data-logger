96c44a70a17b5e6aef1da6efbd6ed570db1a2996
ivvz <ivanwzh.iqm@gmail.com>
2021-11-27 18:59:46 -0600

    se muestran las lecturas del termopar en la pantalla oled y se simula en la salida que se tiene 3 termopares

--- temp_data_logger/temp_data_logger.ino
+++ temp_data_logger/temp_data_logger.ino
@@ -1,18 +1,22 @@
-// this example is public domain. enjoy!
-// https://learn.adafruit.com/thermocouple/
+#include "max6675.h"  
+#include <Adafruit_GFX.h> 
+#include <Adafruit_SSD1306.h>
 
-#include "max6675.h"
 
-int thermoDO = 4;
-int thermoCS = 5;
-int thermoCLK = 6;
+#define OLED_RESET LED_BUILTIN
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+int thermoDO = 7;
+int thermoCS = 6;
+int thermoCLK = 5;
 
 int buttonCreate = 2;
 int buttonClose = 3;
-int led =7;
+int led = 4;
 
 unsigned long termocoupleTime;
-unsigned long readTime = 500;
+unsigned long readTime = 1000;
 
 float temperature;
 
@@ -24,55 +28,70 @@ MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 void setup() {
   Serial.begin(9600);
 
-  pinMode(buttonCreate, INPUT_PULLUP); 
-  pinMode(buttonClose, INPUT_PULLUP);
-  digitalWrite(buttonCreate, HIGH);
-  digitalWrite(buttonClose, HIGH);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  //display.drawBitmap(0,0,logo,128,64,1);
+  //display.display();
+  delay(1000);
 
-  pinMode(led, OUTPUT);
-  
-
-
-  lastStateCreateButton = digitalRead(buttonCreate);
-  lastStateCloseButton = digitalRead(buttonClose);
-  
   Serial.println("MAX6675 correct");
-  // wait for MAX chip to stabilize
   delay(500);
+  // wait for MAX chip to stabilize
+
+  display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(15,20);
+  display.println("TERMOPAR");
+  display.setCursor(50,40);
+  display.setTextSize(3);
+  display.println("OK");
+  display.display();
+  delay(1000);
+  
   
 }
 
 void loop() {
 
+  float temp_1, temp_2, temp_3; // Suponiendo 3 termopares
+  
   if(millis() - termocoupleTime >= readTime){
-    continuousTermocoupleRead();  
+    temp_1 = readTemperature(1);
+    temp_2 = readTemperature(2);
+    temp_3 = readTemperature(3);
+    showTemp1(temp_1,temp_2,temp_3);
     }
-
-  int buttonState = digitalRead(buttonCreate);
-  
-  if(digitalRead(buttonState == LOW)){
-   //Serial.println("no apretaste el boton");
-   digitalWrite(led, LOW);
-  } else{
-    digitalWrite(7,HIGH);
-    Serial.println("creatting file");
-    delay(1000);
-  }
-
-
-
 }
 
-void continuousTermocoupleRead(){
-   Serial.print("C = "); 
-   readTemperature();
-   //delay(250);
-   termocoupleTime = millis();
-   
-}
 
-void readTemperature(){
+
+
+float readTemperature(int termopar){
   temperature = thermocouple.readCelsius();
-  Serial.println(temperature);
+  Serial.println("Termopar " + String(termopar) + " = " + String(temperature) + " C"); 
+  termocoupleTime = millis();   
+  return temperature;
+}
+
+
+void showTemp1(float temperature1,float temperature2,float temperature3) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,5);
+  display.println("tp1");
+  display.setCursor(0,30);
+  display.println("tp2");
+  display.setCursor(0,55);
+  display.println("tp3");
   
+  display.setTextSize(2);
+  display.setCursor(25,0);
+  display.println(String(temperature1) + " C");
+  display.setCursor(25,25);
+  display.println(String(temperature2) + " C");
+  display.setCursor(25,50);
+  display.println(String(temperature3) + " C");
+  display.display();
+  delay(1000);
 }
